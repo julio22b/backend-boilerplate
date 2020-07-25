fd913c111e821dc0490216be4ace7a61ce81c031
Julio Bermudez <jbermudezfc90@gmail.com>
2020-07-25 13:31:44 -0400

    test config working properly

---
+++ src/mongoConfig.ts
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+void mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
